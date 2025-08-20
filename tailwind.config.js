53a282581d7121ee68d01acde120d75b1cc4e82f
Volkan Şah <VolkanSah@users.noreply.github.com>
2025-08-20 22:04:06 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Sagt Tailwind, dass es alle deine HTML-Dateien im Hauptverzeichnis nach Klassen durchsuchen soll
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
